777f3fcb08db36c41a140c4777c568320d73bad7
JulianHernandez1944 <julhernandezar@unal.edu.co>
2025-05-13 16:06:18 -0500

    preprocess lines okey

--- homework/src/wordcount.py
+++ homework/src/wordcount.py
@@ -1,37 +1,31 @@
 # obtain a list of files in the input directory
 import os
 
+from homework.src._internals.preprocess_lines import preprocess_lines
+from homework.src._internals.read_all_lines import read_all_lines
 from homework.src._internals.write_count_words import write_count_words
 
 
 def main():
 
-    # Leer las líneas
-    all_lines = []
-    input_file_list = os.listdir("data/input/")
-    for filename in input_file_list:
-        file_path = os.path.join(
-            "data/input", filename
-        )  # une la ruta del directorio con el nombre del archivo
-        with open(file_path, "r", encoding="utf-8") as f:
-            lines = f.readlines()
-            all_lines.extend(
-                lines
-            )  # extend() agrega los elementos de la lista lines a la lista all_lines
+    # Leer las líneas_Mover a read_all_lines.py
+    all_lines = (
+        read_all_lines()
+    )  # extend() agrega los elementos de la lista lines a la lista all_lines
 
-    # Procesado de las líneas
-    all_lines = [
-        line.lower().strip() for line in all_lines
-    ]  # strip() elimina los espacios en blanco al principio y al final de la línea
+    # Procesado de las líneas__ mover a preprocess_lines.py
+    all_lines = preprocess_lines(
+        all_lines
+    )  # strip() elimina los espacios en blanco al principio y al final de la línea
 
-    # Separar las líneas en palabras
+    # Separar las líneas en palabras mover a split_in_words.py
     words = []
     for line in all_lines:
         words.extend(
             word.strip(",.!?") for word in line.split()
         )  # split() divide la línea en palabras y strip() elimina los signos de puntuación
 
-    # conteo
+    # conteo mover a count_words.py
     counter = {}
     for word in words:
         counter[word] = (
@@ -52,4 +46,4 @@ def main():
 
 
 if __name__ == "__main__":
-    main()  # hace la llamada a la funcion main
+    main()
